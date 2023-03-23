ccf805212f4e0acd0f8493936ce20f156d1c5398
KevinZucca <kev.94@live.it>
2023-03-23 17:06:36 +0100

    feat: editings

---
+++ src/components/AppHero.vue
@@ -0,0 +1,30 @@
+<script>
+  export default {
+    data () {
+    },
+
+  }
+</script>
+
+
+
+
+    
+<template>
+ <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <img src="/images/heroes.jpg" alt="heroes img">
+    </div>
+  </div>
+</template>
+
+
+
+<style>
+  .jumbotron {
+    background-color: #fff;
+    width: 200px;
+    height: 200px;
+  }
+</style>
+    
