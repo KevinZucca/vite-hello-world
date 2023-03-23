60175f7f6da2105b3353952bfa379eefa3228cd2
KevinZucca <kev.94@live.it>
2023-03-23 16:25:19 +0100

    feat: first steps in vite, title added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  import Title from "./components/AppTitle.vue"
+
+
+  export default {
+    data() {
+      return {
+
+      };
+    },
+    components: {
+      Title,
+    },
+
+    methods: {}
+
+  };
 </script>
 
 <template>
   <div>
+    <Title></Title>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -11,7 +27,6 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+
+
+
+    
+<template>
+  <h1> La mia prima app con Vite!</h1>
+</template>
+
+
+
+<style>
+    h1 {
+        font-size: 30px;
+    }
+</style>
+    
+    
+    
